df528ac51be8c591ce05bf46b96b61f8651bd567
Clayton Collie <clayton.collie@gmail.com>
2015-12-18 13:25:55 -0600

    login logo fixed

--- lib/maidive-login-logo.php
+++ lib/maidive-login-logo.php
@@ -6,9 +6,9 @@ function sm_login_logo() {
 	?>
 		<style type="text/css">
             .login h1 a {
-                background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/login-logo.png);
-                padding-bottom: 50px;
-                background-size: 100%;
+                background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo-color.png);
+                padding-bottom: 135px;
+                background-size: inherit;
                 width: 100%;
                 height: 100%;
             }
